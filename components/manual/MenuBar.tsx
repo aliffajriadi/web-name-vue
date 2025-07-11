e2f01fe49ead667e40d53d6ed12eddefb2799b66
aliffajriadi <aliffajriadi@gmail.com>
2025-07-12 01:35:52 +0700

    add1

--- components/manual/MenuBar.tsx
+++ components/manual/MenuBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { toast } from "sonner";
-import { igdata, lkdata, gitdata, emaildata } from "@/lib/data";
+import { igdata, lkdata, gitdata } from "@/lib/data";
 
 
 import {
